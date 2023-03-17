d8cc235934de28d896af1bc1c1ee16d39941cf51
Fareha <hfariha104@gmail.com>
2023-03-17 18:23:45 +0500

    add new buttons and some functionallity to them

--- lib/main.dart
+++ lib/main.dart
@@ -47,26 +47,88 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var no1 = TextEditingController();
+  var no2 = TextEditingController();
+  var result = "";
 
-  var count = 0;
+  // var count = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Stateful"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-             Text('Count : $count'),
-            ElevatedButton(onPressed: () {
-              setState(() {       //set state is used to show result at our screen whenever screen reloads
-                count++;
-                print(count);
-              });
-            }, child: const Text("Increment Counter"))
-          ],
+      body: Container(
+        color: Colors.blue.shade100,
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                  keyboardType: TextInputType.number,
+                  controller: no1 ,
+                ),
+                TextField(
+                  keyboardType: TextInputType.number,
+                  controller: no2,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(21.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(onPressed: (){
+                        int num1 = int.parse(no1.text.toString());
+                        int num2 = int.parse(no2.text.toString());
+
+                        var sum = num1 + num2 ;
+                        result = "The sum of $num1 and $num2 is $sum";
+                        setState(() {
+
+                        });
+                      }, child: Text("Add")),
+                      ElevatedButton(onPressed: (){
+                        int num1 = int.parse(no1.text.toString());
+                        int num2 = int.parse(no2.text.toString());
+
+                        var diff = num1 - num2;
+                        result = "The subtraction of $num1 and $num2 is $diff";
+                        setState(() {
+
+                        });
+                      }, child: Text("Subtract")),
+                      ElevatedButton(onPressed: (){
+                        int num1 = int.parse(no1.text.toString());
+                        int num2 = int.parse(no2.text.toString());
+
+                        var mult = num1*num2;
+                        result="The multiplication of $num1 and $num2 is $mult";
+                        setState(() {
+
+                        });
+
+                      }, child: Text("multiply")),
+                      ElevatedButton(onPressed: (){
+                        int num1 = int.parse(no1.text.toString());
+                        int num2 = int.parse(no2.text.toString());
+
+                        var div = num1/num2;
+                        result="The division of $num1 and $num2 is $div";
+                        setState(() {
+
+                        });
+
+                      }, child: Text("divide")),
+                    ],
+                  ),
+                ),
+                Padding(padding: const EdgeInsets.all(8.0),
+                child: Text(result , style: TextStyle(fontSize: 25),),)
+              ],
+            ),
+          ),
         ),
       ),
     );
