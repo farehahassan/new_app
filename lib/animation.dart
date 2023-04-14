726ac7099d4b94dacb383f4a4e6f413f8a7e141a
Fareha <hfariha104@gmail.com>
2023-04-14 20:26:22 +0500

    FOO animation

--- lib/animation.dart
+++ lib/animation.dart
@@ -8,18 +8,58 @@ class animation extends StatefulWidget {
 }
 
 class _MyAnimationState extends State<animation> {
+
+  var _width = 100.0;
+  var _height = 200.0;
+  Decoration myDecor = BoxDecoration(
+    borderRadius: BorderRadius.circular(10),
+    color: Colors.orangeAccent
+  );
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-      ),
-      body: Container(
-        width: 100,
-        height: 100,
-        color: Colors.purple,
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        AnimatedContainer(
+          width: _width,
+            height: _height,
+            curve: Curves.bounceOut,
+            decoration: myDecor,
+            duration: Duration(seconds: 2)
+        ),
+        SizedBox(
+          height: 30,
+        ),
+        ElevatedButton(onPressed: (){
+
+          if(_width == 100.0 && _height == 200.0){
+            var temp = _width;
+            _width = _height;
+            _height = temp;
+
+            myDecor = BoxDecoration(
+              borderRadius: BorderRadius.circular(50),
+                    color : Colors.orangeAccent
+
+            );
+          }
+          else if(_width==200.0 && _height == 100.0 ){
+            var temp = _width;
+            _width = _height;
+            _height = temp;
+
+            myDecor = BoxDecoration(
+                borderRadius: BorderRadius.circular(50),
+                color : Colors.red
+            );
+
+          }
+          setState(() {
+
+          });
+        }, child: Text("Animate"))
+
+      ],
     );
   }
 }
